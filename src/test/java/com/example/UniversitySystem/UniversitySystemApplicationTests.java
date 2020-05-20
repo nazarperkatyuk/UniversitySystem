13e6c95a90d37bd89c9f96203f00ac068c37bec9
nazarperkatyuk <perkatyuk.n@gmail.com>
2020-05-20 17:43:40 +0300

    init project

---
+++ src/test/java/com/example/UniversitySystem/UniversitySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UniversitySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
